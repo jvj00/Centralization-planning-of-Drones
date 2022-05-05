cfd1800fa902e2d8e6e2a4ce57e18a20dc7a4ed5
JvJ00 <manuel.righele@studenti.unitn.it>
2022-05-05 11:57:15 +0200

    Added json output; Added lowe level heights (offset); Added more detailed description of help

--- points_links_gen.cpp
+++ points_links_gen.cpp
@@ -49,9 +49,9 @@ int main(int argc, char** argv)
     int h, w, l, ntarget=0, index_arguments=1;
     bool pddl=false, ps2=false, json=false;
     char *pddl_file, *ps2_file, *json_file;
-    double dst_pts, dst_lvl;
+    double dst_pts, dst_lvl, low_lvl;
     string points[MAX_LEVELS][MAX_HEIGHT][MAX_WIDTH];
-    string out_l="",out_d="", out_list_p="";
+    string out_l="",out_d="", out_list_p="", out_coordinates="";
     string d1_pos="", d2_pos="", d1_dir="", d2_dir="", targets[MAX_VISIT_POINTS], directions[MAX_VISIT_POINTS];
 
     //PARSING ARGUMENTS
@@ -78,13 +78,14 @@ int main(int argc, char** argv)
         if(!json_file) common_error();
     }
     //PARAMETERS
-    if(argc>=11+index_arguments && argc%2==0 && is_number_int(argv[index_arguments]) && is_number_int(argv[1+index_arguments]) && is_number_int(argv[2+index_arguments]) && is_number(argv[3+index_arguments]) && is_number(argv[4+index_arguments]))
+    if(argc>=12+index_arguments && argc%2==1 && is_number_int(argv[index_arguments]) && is_number_int(argv[1+index_arguments]) && is_number_int(argv[2+index_arguments]) && is_number(argv[3+index_arguments]) && is_number(argv[4+index_arguments]) && is_number(argv[5+index_arguments]))
     {
         h = atoi(argv[index_arguments++]);
         w = atoi(argv[index_arguments++]);
         l = atoi(argv[index_arguments++]);
         dst_pts = stod(argv[index_arguments++]);
         dst_lvl = stod(argv[index_arguments++]);
+        low_lvl = stod(argv[index_arguments++]);
         d1_pos = argv[index_arguments++];
         d1_dir = argv[index_arguments++];
         d2_pos = argv[index_arguments++];
@@ -99,8 +100,20 @@ int main(int argc, char** argv)
     //HELP & ERRORS
     else if(argc==2 && (!((string)argv[1]).compare("--help") || !((string)argv[1]).compare("-h")))
     {
-        cout << "This tool generate a 2D map of points, giving height and width in points as input." << endl;
-        cout << "Usage: ./map_tool [options] <height(pts)> <width(pts)> <#levels> <distance_pts(m)> <distance_lvl(m)> <drone1_position> <drone1_direction> <drone2_position> <drone2_direction> <target1 direction1 target2 direction2 ...>" << endl;
+        cout << "This tool generate a 2D map of points, giving height and width in points as input." << endl << endl;
+        cout << "Usage: ./map_tool [options] <height> <width> <levels> <distance_pts> <distance_lvl> <lowest_lvl> <drone1_position> <drone1_direction> <drone2_position> <drone2_direction> <target1 direction1 target2 direction2 ...>" << endl;
+        cout << endl;
+        cout << "Parameters:" << endl;
+        cout << "   <height(pts)> height 2D (y) in points of the map" << endl;
+        cout << "   <width(pts)> width 2D (x) in points of the map" << endl;
+        cout << "   <levels> # of levels (z) of the map" << endl;
+        cout << "   <distance_pts> distance in meters between points of the same level" << endl;
+        cout << "   <distance_lvl> distance in meters between levels" << endl;
+        cout << "   <lowest_lvl> heigth in meters of the lowest level" << endl;
+        cout << "   <drone1_position> <drone1_direction> position (name of point) and direction (name of direction) of the first drone. E.g.: p23 dir45" << endl;
+        cout << "   <drone2_position> <drone2_direction> same, but with second drone" << endl;
+        cout << "   <target1 direction1 target2 direction2 ...> list of target position and direction of the planning. E.g.: p45 dir180 p89 dir225" << endl;
+        cout << endl;
         cout << "Options:" << endl;
         cout << "   --pddl <path_to_out_file>          generates a pddl problem as output" << endl;
         cout << "   --ps2 <path_to_out_file>           generates a plansys2 problem as output" << endl;
@@ -116,7 +129,12 @@ int main(int argc, char** argv)
     for(int i=0;i<l;i++)
         for(int j=0;j<h;j++)
             for(int k=0;k<w;k++)
+            {
                 points[i][j][k] = "p" + to_string((i*w*h)+(j*w)+k);
+                out_coordinates += "{\"name\":\"" + points[i][j][k] + "\",\"x\":" + to_string(k*dst_pts) + ",\"y\":" + to_string(j*dst_pts) + ",\"z\":" + to_string(i*dst_lvl+low_lvl) + "}";
+                if(i!=l-1 || j!=h-1 || k!=w-1)
+                    out_coordinates += ",";
+            }
 
     for(int i=0;i<l;i++)
         for(int j=0;j<h;j++)
@@ -337,10 +355,11 @@ int main(int argc, char** argv)
     }
 
     //WRITE ON JSON FILE
-    if(ps2)
+    if(json)
     {
         ofstream json_out;
         json_out.open(json_file);
+        json_out << "{\"points\":[" << out_coordinates << "]}";
     }
 
 
